0b5cacc1fd16df6c528949a6f23d9a6c77559677
Beau Costoff <wbeaumont@rogers.com>
2016-06-09 15:26:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    //res.send('Hello world, I am a chat bot')
-    res.send('1876115476')
+    res.send('Hello world, I am a chat bot')
 })
 
 // for Facebook verification
