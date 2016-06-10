2931cb3b68a85aa52976d0e2904e8590e3cf4285
Beau Costoff <wbeaumont@rogers.com>
2016-06-10 08:24:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function sendTextMessage(sender, text) {
         text:text
     }
     request({
-        url: 'https://graph.facebook.com/v2.6/me/subscribed_apps',
+        url: 'https://graph.facebook.com/v2.6/me/...',
         qs: {access_token:token},
         method: 'POST',
         json: {
