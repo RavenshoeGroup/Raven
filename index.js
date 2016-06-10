ec433d85f2f1df29f3ece3257fa350d401bf3f8e
Beau Costoff <wbeaumont@rogers.com>
2016-06-10 10:11:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -89,58 +89,23 @@ function sendGenericMessage(sender) {
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "Ai Chat Bot Communities",
-                    "subtitle": "Communities to Follow",
-                    "image_url": "http://1u88jj3r4db2x4txp44yqfj1.wpengine.netdna-cdn.com/...",
+                    "title": "Need Marketing Advise?",
+                    "subtitle": "Check Us Out",
+                    "image_url": "http://rsg.ravenshoegroup.netdna-cdn.com/_images/rsg-logo-retina.png",
                     "buttons": [{
                         "type": "web_url",
-                        "url": "https://www.facebook.com/groups/aic...",
-                        "title": "FB Chatbot Group"
+                        "url": "http://www.ravenshoegroup.com/",
+                        "title": "Ravenshoe Group"
                     }, {
                         "type": "web_url",
-                        "url": "https://www.reddit.com/r/Chat_Bots/",
-                        "title": "Chatbots on Reddit"
+                        "url": "http://www.ravenshoepackaging.com/",
+                        "title": "Ravenshoe Packaging"
                     },{
                         "type": "web_url",
-                        "url": "https://twitter.com/aichatbots",
-                        "title": "Chatbots on Twitter"
+                        "url": "http://ravenshoedigital.com/",
+                        "title": "Ravenshoe Digital"
                     }],
-                }, {
-                    "title": "Chatbots FAQ",
-                    "subtitle": "Aking the Deep Questions",
-                    "image_url": "https://tctechcrunch2011.files.wordpress.com/...",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "What's the benefit?",
-                        "payload": "Chatbots make content interactive instead of static",
-                    },{
-                        "type": "postback",
-                        "title": "What can Chatbots do",
-                        "payload": "One day Chatbots will control the Internet of Things! You will be able to control your homes temperature with a text",
-                    }, {
-                        "type": "postback",
-                        "title": "The Future",
-                        "payload": "Chatbots are fun! One day your BFF might be a Chatbot",
-                    }],
-                },  {
-                    "title": "Learning More",
-                    "subtitle": "Aking the Deep Questions",
-                    "image_url": "http://www.brandknewmag.com/wp-cont...",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "AIML",
-                        "payload": "Checkout Artificial Intelligence Mark Up Language. Its easier than you think!",
-                    },{
-                        "type": "postback",
-                        "title": "Machine Learning",
-                        "payload": "Use python to teach your maching in 16D space in 15min",
-                    }, {
-                        "type": "postback",
-                        "title": "Communities",
-                        "payload": "Online communities & Meetups are the best way to stay ahead of the curve!",
-                    }],
-                }]  
-            } 
+                }
         }
     }
     request({
